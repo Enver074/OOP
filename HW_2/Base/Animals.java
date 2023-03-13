340c97fcc3dbed65334996436d9a99ba95eeec45
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-03-13 16:15:17 +0300

    родительский класс 'животные'

---
+++ HW_2/Base/Animals.java
@@ -0,0 +1,21 @@
+package Base;
+public abstract class Animals {
+    private int height;
+    private int weight;
+    private String colorEye;
+
+    protected Animals(int height, int weight, String colorEye) {
+        this.height = height;
+        this.weight = weight;
+        this.colorEye = colorEye;
+    }
+
+    public abstract void makeSound();
+
+    @Override
+    public String toString() {
+        return String.format("Рост: %d; Вес: %d; Цвет глаз: %s", this.height, this.weight, this.colorEye);
+    }
+
+    
+}
