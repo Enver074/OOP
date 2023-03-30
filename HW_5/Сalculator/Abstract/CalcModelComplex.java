4caabcf940e351c0916d7d973ee456bfe93f9b77
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-03-30 22:11:25 +0300

    Абстрактный класс для комплексных чисел

---
+++ HW_5/Сalculator/Abstract/CalcModelComplex.java
@@ -0,0 +1,10 @@
+package Сalculator.Abstract;
+
+import Сalculator.Interface.ModelComplex;
+
+public abstract class CalcModelComplex implements ModelComplex{
+    protected int x;
+    protected int y;
+    protected int j;
+    protected int k;
+}
