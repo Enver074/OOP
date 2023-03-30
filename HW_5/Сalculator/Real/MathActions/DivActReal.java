4983786c79ce9578b5f4ea63253006be624b9b0e
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-03-30 22:18:16 +0300

    Операция деления для вещественных чисел

---
+++ HW_5/Сalculator/Real/MathActions/DivActReal.java
@@ -0,0 +1,44 @@
+package Сalculator.Real.MathActions;
+
+import java.io.IOException;
+
+import Сalculator.Menu;
+import Сalculator.Abstract.CalcModelReal;
+
+public class DivActReal extends CalcModelReal{
+    public void DivAction(){
+        
+
+    }
+
+    @Override
+    public int result() throws IOException {
+        try {
+            return x / y;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Деление на ноль");
+        }
+        return Menu.menuReal();
+    }
+
+    @Override
+    public void setX(int value) {
+        super.x = value;
+    }
+
+    @Override
+    public void setY(int value) {
+        super.y = value;
+    }
+
+    @Override
+    public String toString() {
+        if(x==0||y==0){
+            return String.format("Делить на ноль нельзя: %d / %d", x, y );  
+        }
+        else{
+            return String.format("%d / %d = %d", x, y, x/y);
+        }       
+    }
+}
